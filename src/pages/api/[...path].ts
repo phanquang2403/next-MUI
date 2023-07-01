0aff73868c1e7593220e6386ff9415334b745501
phanquang <phandangquang2403@gmail.com>
2023-07-01 11:01:12 +0700

    NextJS: 03-04 Proxy requests to API server ��

---
+++ src/pages/api/[...path].ts
@@ -0,0 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import httpProxy from "http-proxy";
+
+const proxy = httpProxy.createProxyServer();
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  return new Promise<void>((resolve) => {
+    //convert cookie to header Authorization
+    // const cookie = new Cookies(req, res);
+    // const accessToken = cookie.get("access_token");
+    // if (accessToken) {
+    //   req.headers.authorization = `Bearer ${accessToken}`;
+    // }
+
+    //don't send cookies to API server
+    req.headers.cookie = "";
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: false, //response  server handle and return
+    });
+
+    proxy.once("proxyRes", () => {
+      resolve();
+    });
+  });
+
+  // res.status(200).json({ name: "PATH - MATH " });
+}
