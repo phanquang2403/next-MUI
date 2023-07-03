32e57efc32a9a32e882ac1bef116c8a3795991fc
phanquang <phandangquang2403@gmail.com>
2023-07-03 11:08:00 +0700

    NextJS: 04-02 Setup MUI v5 cho NextJS Typescript Project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,22 @@ import axiosClient from "@/api-client/axios-client";
 import { Emptylayout } from "@/components/layouts";
 import { AppPropsWithLayout } from "@/models";
 import "@/styles/globals.css";
+import { createEmotionCache, theme } from "@/utils";
+import { EmotionCache } from "@emotion/cache";
+import { CacheProvider, ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { AppProps } from "next/app";
+import Head from "next/head";
 import { SWRConfig } from "swr";
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyAppProps({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: AppPropsWithLayout) {
   // sẽ lấy từ Pagelayout.Layout để wrapper Component lại
   const Layout = Component.Layout ?? Emptylayout;
 
@@ -14,10 +27,18 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   };
 
   return (
-    <SWRConfig value={option}>
-      <Layout>
-        <Component {...pageProps} />;
-      </Layout>
-    </SWRConfig>
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SWRConfig value={option}>
+          <Layout>
+            <Component {...pageProps} />;
+          </Layout>
+        </SWRConfig>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,10 +1,17 @@
 import { Adminlayout, Mainlayout } from "@/components/layouts";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 type Props = {};
 
 const PageAbout = (props: Props) => {
-  return <div>page about</div>;
+  return (
+    <Box>
+      <Typography component={"h1"} variant="h3" color={"primary.main"}>
+        <div>page about</div>
+      </Typography>
+    </Box>
+  );
 };
 
 PageAbout.Layout = Adminlayout;
